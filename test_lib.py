ae146095cc1290694c6079c92cd2f4f2eae4d4b6
SophieS <ssouvanheuane@hotmail.com>
2022-11-02 12:18:01 +0100

    commit test_lib

---
+++ test_lib.py
@@ -0,0 +1,13 @@
+from job import moyenne
+def test_moyenne():
+    data =[1,1,1]
+    expected_result = 1
+
+    result = moyenne(data)
+
+    if result != expected_result :
+        print('error in moyenne')
+        exit(1)
+
+test_moyenne()
+exit(0)
