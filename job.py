c225b7fd5f93f7273b3bf2d44107855b5b0ff513
SophieS <ssouvanheuane@hotmail.com>
2022-11-03 10:58:31 +0100

    test commit3

--- job.py
+++ job.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 def generate_log(params):
 
-    with open('logs.txt', 'a') as f:
+    with open('log.txt', 'a') as f:
 
         f.write('on a appelé la fonction moyenne {} avec les parametres {}\n'.format(
             datetime.now(), params)
